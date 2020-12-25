7a2d1aee6ab5560b5fdfd5d04d7a6df5dc329f12
Shakti Ranjan Debata <debatashaktiranjan@gmail.com>
2020-12-25 22:19:49 +0530

    use / & cake host

--- home/views.py
+++ home/views.py
@@ -5,6 +5,5 @@ def index(request):
     url = getMemeURL()
     myDict = {
         "url" : url,
-        "host" : "http://127.0.0.1:8000"
         }
     return render(request, 'index.html', myDict)
